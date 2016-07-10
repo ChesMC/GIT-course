f3fc0fc319eabca4cc00a300da66ddaf8ba2412a
ChesMC <wolf_zem_@mail.ru>
2016-07-10 16:59:29 +0300

    сообщили, что это курс по GIT

--- C# example/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ C# example/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello, world!!!");
+            Console.Write("Hello, world!!! This is GIT-course");
             Console.ReadLine();
 
         }
